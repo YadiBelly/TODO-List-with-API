639d69b34b540b7c311b44cf9cc49992ce9df4f3
YadiBelly <Yadisabell96@gmail.com>
2022-02-13 15:30:33 +0000

    first commit

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -1,25 +1,80 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const TodoList = () => {
 	const [inputTask, setInputTask] = useState("");
 	const [taskList, setTaskList] = useState([]); // SetTaskList changes value of tasklist
+	const uri = "https://assets.breatheco.de/apis/fake/todos/user/yadisa";
 
-	// (put the function in the code)
-	// function onChange(e) {
-	// 	const newValue = e.target.value;
-	// 	setInputValue(newValue);
-	// }
+	useEffect(() => {
+		getFetch();
+	}, []);
+
+	const getFetch = () => {
+		fetch(uri)
+			.then(function (response) {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then(function (responseAsJson) {
+				console.log("responseAsJson", responseAsJson);
+				setTaskList(responseAsJson);
+			})
+			.catch(function (error) {
+				console.log("error", error);
+			});
+	};
+	const updatePut = (updatedTodos) => {
+		fetch(uri, {
+			method: "PUT",
+			body: JSON.stringify(updatedTodos),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((res) => res.json())
+			.then((response) => {
+				console.log("success", response);
+				getFetch();
+			})
+			.catch((error) => console.error("error", error));
+	};
 
 	const saveTask = (e) => {
+		let newTodos = taskList.concat({
+			label: inputTask,
+			done: false,
+		});
+
 		if (e.keyCode == 13) {
-			setTaskList([...taskList, inputTask]); // this is updating value of tasklist
+			setTaskList(newTodos);
+			updatePut(newTodos);
 			setInputTask(""); // this is clearing out the input text field after enter
 		}
 	};
 
 	const removeTask = (index) => {
-		setTaskList(taskList.filter((tasktToRemove, i) => i != index)); //filter is higher order function. makes new array w/out that one index
+		const newTodos = taskList.filter((tasktToRemove, i) => i != index);
+
+		setTaskList(newTodos);
+		updatePut(newTodos);
 	};
+
+	const markDone = (index) => {
+		const newTodos = taskList.map((task, i) => {
+			if (i == index) {
+				task.done = !task.done;
+				return task;
+			} else {
+				return task;
+			}
+		});
+
+		setTaskList(newTodos);
+		updatePut(newTodos);
+	};
+
 	return (
 		<div className="todo-box">
 			<h1>todos</h1>
@@ -37,14 +92,22 @@ export const TodoList = () => {
 					return (
 						<li className="list" key={index}>
 							{" "}
-							{/*must have key when using map function*/}
-							{task}
+							<span className={task.done ? "strike" : ""}>
+								{task.label}
+							</span>
 							<span
 								className="delete-icon"
 								onClick={() => removeTask(index)}>
 								{" "}
 								{/*all we care about is the index to remove task not the event itself. we are not trying to save any value by the onclick*/}
 								<i className="fas fa-trash"></i>
+							</span>
+							<span
+								onClick={() => markDone(index)}
+								className={task.done ? "green" : ""}>
+								{" "}
+								{/*all we care about is the index to remove task not the event itself. we are not trying to save any value by the onclick*/}
+								<i className="fas fa-check-square"></i>
 							</span>
 						</li>
 					);
